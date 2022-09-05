8f26874d2807574b78647db7b21ea4faec2e3bb4
Caren Daniel <caren.daniel@studmail.w-hs.de>
2022-09-05 13:54:35 +0200

    changed run.sh

--- run.sh
+++ run.sh
@@ -9,15 +9,15 @@ then
     nohup python3 ./temp/syntrans_masked.py > ./logs/syntrans_masked.log & echo $!
 fi
 
-if [ $1 == "ner" ];
+if [ $1 == "syngnn" ];
 then
-    rm ./temp/syntrans_ner.py
-    rm ./logs/syntrans_ner.log
-    jupyter nbconvert --output-dir='./temp' --to script ./syntrans_ner.ipynb
-    nohup python3 ./temp/syntrans_ner.py > ./logs/syntrans_ner.log & echo $!
+    rm ./temp/syntrans_main.py
+    rm ./logs/syntrans_main.log
+    jupyter nbconvert --output-dir='./temp' --to script ./syntrans_main.ipynb
+    nohup python3 ./temp/syntrans_ner.py > ./logs/syntrans_main.log & echo $!
 fi
 
-if [ $1 == "preprocessing" ];
+if [ $1 == "preprocessing_ud" ];
 then
     rm ./logs/data_preprocessing_dep_ud.log
     nohup python3 ./data_preprocessing_dep_ud.py > ./logs/data_preprocessing_ud.log & echo $!
