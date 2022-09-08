3b6ab2e7b9f4b1dee81b15b75384f1748e1b8ddc
Caren Daniel <caren.daniel@studmail.w-hs.de>
2022-09-08 15:33:13 +0200

    bugfix in graph loading

--- ner_tagging.py
+++ ner_tagging.py
@@ -113,7 +113,7 @@ def CreateNERLabelsFromDataset(file, tagger):
 
 
 # Load text files
-data_path = "./data/ud/UD_English-GUM/"
+data_path = "./data/ud/UD_English-LinES/"
 # Files in data folder to ignore
 skip_files = []
 files = glob.iglob(data_path + '**/en_*.txt', recursive=True)
